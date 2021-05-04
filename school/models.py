3f2c4a2a9d61fd789df76d622e7a625c3bf15bd3
AnnaMrowca <amrowca@gmail.com>
2021-05-04 18:05:15 +0200

    creating models, initiating migrations

--- school/models.py
+++ school/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    students = models.ManyToManyField(Student,related_name='subjects')
+
+
+    def __str__(self):
+        return self.name
+
+class Status:
+    PRESENT = 'present'
+    COVID = 'covid'
+    ABSENT = 'absent'
+
+    CHOICES = [(PRESENT, 'present'), (COVID,'covid'), (ABSENT, 'absent')]
+
+class PresenceStatus(models.Model):
+    person = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject_class = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+    presence_status = models.CharField(max_length=7, choices= Status.CHOICES)
+
+    def __str__(self):
+        return f'{self.person.name} {self.person.surname} {self.subject_class} {self.presence_status}'
